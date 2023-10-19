efde95cb3e75eb703acefb7295392b26992fc117
REVISION-PC\eric_laptop <eric@escrich.net>
2023-10-19 20:16:13 +0200

    rama 4

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>50x15</title>
+</head>
+<body>
+    <div>
+        <img src="" alt="">
+    </div>
+    <h1></h1>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+    <a href="game.php"></a>
+    <a href="ranking.php"></a>
+
+    <div id="languages">
+        <img id="ca" src="" alt="Catalan flag">
+        <img id="es" src="" alt="Spanish flag">
+        <img id="en" src="" alt="England flag">
+    </div>
+</body>
+</html>
