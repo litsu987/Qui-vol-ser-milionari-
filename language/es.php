97e8f33377a6d3dba545c5e7341ea781fe7da329
eric_escrich <eric@escrich.net>
2023-10-20 18:13:05 +0200

    language files

---
+++ language/es.php
@@ -0,0 +1,28 @@
+<?php
+$lang = array(
+    'instructions' => [
+        "El concursante comienza en la pregunta número 1 y debe responder correctamente para avanzar a la siguiente pregunta.",
+        "El concursante solo puede ver la pregunta en la que se encuentra actualmente y las preguntas que ya ha respondido.",
+        "Una vez que el concursante ha respondido a una pregunta, no puede modificarla.",
+        "Cada 3 respuestas correctas, el jugador avanzará al siguiente nivel. Las preguntas se vuelven más difíciles en cada nivel. Hay 6 niveles en total.",
+        "Si el concursante responde incorrectamente a una pregunta, el juego termina.",
+        "El objetivo final es llegar a la pregunta número 18 y responderla correctamente para ganar el premio máximo."
+    ],
+    'buttons' => [
+        'play' => 'JUGAR',
+        'nextQuestions' => 'SIGUIENTES PREGUNTAS',
+        'toStart' => 'VOLVER AL INICIO',
+        'publishScore' => '¿Quieres publicar tu puntuación?',
+        'publishButton' => 'PUBLICAR PUNTUACIÓN',
+        'hallOfFameButton' => 'Hall of Fame'
+    ],
+    'messages' => [
+        'correct' => '¡Respuesta correcta!',
+        'incorrect' => '¡Respuesta incorrecta!',
+        'win' => 'FELICIDADES, ¡HAS GANADO!',
+        'lose' => 'LO SIENTO, HAS PERDIDO'
+    ],
+    'inputPlaceholder' => 'Introduce tu nombre',
+    'tittle' => '¿QUIEN QUIERE SER MILLONARIO?'
+);
+?>
