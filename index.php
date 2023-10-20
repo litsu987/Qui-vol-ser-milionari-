f53a700b26ed1d56071287aa0343affc59f55743
Eric Escrich <95907445+eric-escrich@users.noreply.github.com>
2023-10-20 17:06:13 +0200

    Add files via upload
    languages and index.php

--- index.php
+++ index.php
@@ -1,30 +1,68 @@
+<?php
+session_start();
+if (isset($_GET['lang']) && ($_GET['lang'] == 'es' || $_GET['lang'] == 'ca' || $_GET['lang'] == 'en')) {
+    $_SESSION['lang'] = $_GET['lang'];
+} else {
+    $_SESSION['lang'] = 'en';
+}
+include 'language/' . $_SESSION['lang'] . '.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>50x15</title>
+    <title>
+        <?php echo $lang['tittle']; ?>
+    </title>
 </head>
+
 <body>
     <div>
         <img src="" alt="">
     </div>
-    <h1></h1>
+    <h1>
+        <?php echo $lang['tittle']; ?>
+    </h1>
     <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+            <?php echo $lang['instructions']['0']; ?>
+        </li>
+        <li>
+            <?php echo $lang['instructions']['1']; ?>
+        </li>
+        <li>
+            <?php echo $lang['instructions']['2']; ?>
+        </li>
+        <li>
+            <?php echo $lang['instructions']['3']; ?>
+        </li>
+        <li>
+            <?php echo $lang['instructions']['4']; ?>
+        </li>
     </ul>
 
-    <a href="game.php"></a>
-    <a href="ranking.php"></a>
+    <a href="game.php">
+        <?php echo $lang['buttons']['play']; ?>
+    </a>
+    <a href="ranking.php">
+        <?php echo $lang['buttons']['hallOfFameButton']; ?>
+    </a>
 
     <div id="languages">
-        <img id="ca" src="" alt="Catalan flag">
-        <img id="es" src="" alt="Spanish flag">
-        <img id="en" src="" alt="England flag">
+        <a href="index.php?lang=ca">
+            <img id="ca" src="images/cataloniaFlag.jpg" alt="Catalan flag">
+        </a>
+        <a href="index.php?lang=es">
+            <img id="es" src="images/spainFlag.jpg" alt="Spanish flag">
+        </a>
+        <a href="index.php?lang=en">
+            <img id="en" src="images/UKFlag.jpg" alt="England flag">
+        </a>
+
     </div>
 </body>
+
 </html>
