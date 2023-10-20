6a16d0a2f9989eaddebf76af2a9fe0e9a3b46a3c
R0sac <oscarhortelano2001@gmail.com>
2023-10-20 17:16:53 +0200

    sonido
    sonidos juego

---
+++ juego.js
@@ -0,0 +1,30 @@
+function soundBadQuestion(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","./music/error_sound.mp3");
+	document.body.appendChild(sonido);
+}
+function soundSuccessQuuestion(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","./music/success_sound.mp3");
+	document.body.appendChild(sonido);
+}
+function soundHelp(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","./music/help_sound.mp3");
+	document.body.appendChild(sonido);
+}   
+function soundHelp(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","./music/help_sound.mp3");
+	document.body.appendChild(sonido);
+}   
+function soundWin(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","./music/win_sound.mp3");
+	document.body.appendChild(sonido);
+}   
+function soundLose(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","./music/lose_sound.mp3");
+	document.body.appendChild(sonido);
+}   
